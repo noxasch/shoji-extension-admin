156afbf7688582d3048bc4df85882f48a472e9aa
Alexander Dischberg <noxasch@gmail.com>
2021-07-27 20:13:06 +0800

    feat: refactor getDev to management lib

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,17 +1,5 @@
-import management from '../lib/management';
+import { management, getDevExtension } from '../lib/management';
 import View from './view';
-
-/**
- * 
- * @param {chrome.management.ExtensionInfo[]} extensions
- * @returns {chrome.management.ExtensionInfo[]} devExtensions
- */
-function getDevExtension(extensions) {
-  const extension = extensions;
-  const devExtensions = extension
-    .filter((item) => item.installType === 'development');
-  return devExtensions;
-}
 
 async function main() {
   const extensions = await management.getAll();
