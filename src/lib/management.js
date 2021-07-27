e0a5f20853c8fd2350d6235b528031c33582d988
Alexander Dischberg <noxasch@gmail.com>
2021-07-27 20:11:57 +0800

    feat: add getSelf, reload, getDevExt and reloadAll

--- src/lib/management.js
+++ src/lib/management.js
@@ -2,7 +2,7 @@
  * 
  * @typedef {Object} management
  */
-const management = {
+export const management = {
   /**
    * Get all extensions and app installed
    * @returns {Promise<chrome.management.ExtensionInfo[]>} extensions
@@ -34,6 +34,20 @@ const management = {
 
   /**
    * 
+   * @returns {Promise<chrome.management.ExtensionInfo>}
+   */
+  getSelf: async () => new Promise((resolve, reject) => {
+    chrome.management.getSelf((result) => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+        return;
+      }
+      resolve(result);
+    });
+  }),
+
+  /**
+   * 
    * @param {String} id 
    * @param {boolean} enabled 
    * @returns {Promise<void>}
@@ -49,4 +63,38 @@ const management = {
   }),
 };
 
-export default management;
+/**
+ * 
+ * @param {String} extensionId 
+ */
+export async function reload(extensionId) {
+  await management.setEnabled(extensionId, false);
+  await management.setEnabled(extensionId, true);
+  // show feedback
+}
+
+/**
+ * @param {chrome.management.ExtensionInfo[]} extensions
+ * @returns {chrome.management.ExtensionInfo[]} devExtensions
+ */
+export function getDevExtension(extensions) {
+  const extension = extensions;
+  const devExtensions = extension
+    .filter((item) => item.installType === 'development');
+  return devExtensions;
+}
+
+/**
+ * @param {?chrome.management.ExtensionInfo[]} extensions
+ * @returns {Promise<Void>}
+ */
+export async function reloadAllDev(extensions) {
+  const exts = extensions ?? await management.getAll();
+  const enabledDevExtensions = getDevExtension(exts)
+    .filter((item) => item.enabled);
+  let devIds = enabledDevExtensions.map((item) => item.id);
+  devIds = devIds.filter((id) => id !== chrome.runtime.id);
+  devIds.forEach(async (id) => {
+    if (id) await reload(id);
+  });
+}
