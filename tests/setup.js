4c1cd7ae8979b133d07a900824e577806b6a94ab
Alexander Dischberg <noxasch@gmail.com>
2021-07-29 03:23:04 +0800

    test: update integration test

--- tests/setup.js
+++ tests/setup.js
@@ -14,12 +14,6 @@ function setupTestEnvironment() {
   });
   Object.defineProperty(window, 'window', { value: dom.window });
   Object.defineProperty(document, 'document', { value: dom.window.document });
-  // console.log(document);
-  // dom.window.document.body.innerHTML =`<div id="test" class="test"></div>`;
-  // console.log('querySelector', dom.window.document.querySelector('.test'));
-  // console.log('querySelectorAll', dom.window.document.querySelectorAll('.test'));
-  // console.log('getElementsByClassName', dom.window.document.getElementsByClassName('test'));
-  // console.log('id', dom.window.document.querySelector('#test'));
 }
 
 setupTestEnvironment();
--- tests/viewReloadButton.spec.js
+++ tests/viewReloadButton.spec.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const pti = require('puppeteer-to-istanbul');
 const ViewReloadButton = require('../src/popup/viewReloadButton');
 
-// require('expect-puppeteer');
+// require('expect-puppeteer'); // for custon puppeteer setup
 
 const extensionID = process.env.EXTENSION_ID;
 
@@ -14,22 +14,9 @@ function delay(time) {
 
 describe('', () => {
   // this.timeout = '10000';
-  beforeAll(async () => {
-    await page.coverage.startJSCoverage();
-    await page.coverage.startCSSCoverage();
-  }, 5000);
-
-  afterAll(async () => {
-    const [jsCoverage, cssCoverage] = await Promise.all([
-      page.coverage.stopJSCoverage(),
-      page.coverage.stopCSSCoverage(),
-    ]);
-    pti.write([...jsCoverage, ...cssCoverage], {
-      storagePath: './.nyc_output',
-    });
-  });
-
   beforeEach(async () => {
+    await page.coverage.startJSCoverage();
+
     await page
       .goto(`chrome-extension://${extensionID}/popup.html`);
     // page.on('console', (msg) => {
@@ -37,8 +24,50 @@ describe('', () => {
     //     console.log(`${i}: ${msg.args()[i]}`);
     //   }
     // });
+
+    // await page.coverage.startCSSCoverage();
+    // const targets = await browser.targets();
+    // const backgroundPageTarget = targets.find(
+    //   (target) => target.type() === 'background_page',
+    // );
+    // const backgroundPage = await backgroundPageTarget.page();
+    // await backgroundPage.coverage.startJSCoverage();
+    // backgroundPage.on('console', (msg) => {
+    //   for (let i = 0; i < msg.args().length; i += 1) {
+    //     console.log(`bg ${i}: ${msg.args()[i]}`);
+    //   }
+    // });
+
     jest.setTimeout(10000);
   }, 5000);
+
+  afterEach(async () => {
+    // const targets = await browser.targets();
+    // const backgroundPageTarget = targets.find(
+    //   (target) => target.type() === 'background_page',
+    // );
+    // const backgroundPage = await backgroundPageTarget.page();
+
+    const [jsCoverage] = await Promise.all([
+      page.coverage.stopJSCoverage(),
+      // page.coverage.stopCSSCoverage(),
+      // backgroundPage.coverage.stopJSCoverage(),
+    ]);
+    pti.write([...jsCoverage], {
+      storagePath: './.nyc_output',
+    });
+  });
+
+  // beforeEach(async () => {
+  //   await page
+  //     .goto(`chrome-extension://${extensionID}/popup.html`);
+  //   page.on('console', (msg) => {
+  //     for (let i = 0; i < msg.args().length; i += 1) {
+  //       console.log(`${i}: ${msg.args()[i]}`);
+  //     }
+  //   });
+  //   jest.setTimeout(10000);
+  // }, 5000);
 
   test('test popup', async () => {
     await expect(page.title()).resolves.toMatch('Shoji Extension - Popup');
@@ -46,12 +75,16 @@ describe('', () => {
       .$$(`#${ViewReloadButton.default.reloadBtnId}`);
     expect(inputElement).toBeTruthy();
     expect(inputElement.length).toBe(1);
+    await expect(page.$('.mdi-spin')).resolves.toBeFalsy();
+    await expect(page.$('.mdi-reload')).resolves.toBeTruthy();
     await page.click(`#${ViewReloadButton.default.reloadBtnId}`);
-    const spinner = await page.$('.mdi-loading');
-    // expect(spinner).toBeTruthy();
-    await delay(5000);
-    expect(spinner).toBeFalsy();
-  }, 10000);
+    await delay(500);
+    await expect(page.$('.mdi-reload')).resolves.toBeFalsy();
+    await expect(page.$('.mdi-spin')).resolves.toBeTruthy();
+    await delay(2000);
+    await expect(page.$('.mdi-spin')).resolves.toBeFalsy();
+    await expect(page.$('.mdi-reload')).resolves.toBeTruthy();
+  }, 20000);
 
   test('test popup', async () => {
     const listItem = await page
@@ -72,9 +105,9 @@ describe('', () => {
     );
     const backgroundPage = await backgroundPageTarget.page();
     expect(backgroundPage).toBeTruthy();
-    await backgroundPage.keyboard.down('Alt');
-    await backgroundPage.keyboard.press('R');
-    await backgroundPage.keyboard.up('Alt');
+    await page.keyboard.down('Alt');
+    await page.keyboard.press('R');
+    await page.keyboard.up('Alt');
     await delay(5000);
   }, 10000);
 });
