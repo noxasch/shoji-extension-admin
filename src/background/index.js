5d43ff223ac5cc13337cddb3836f8785ab754048
Alexander Dischberg <noxasch@gmail.com>
2021-08-02 19:29:30 +0800

    feat: add reload tab function in shortcut

--- src/background/index.js
+++ src/background/index.js
@@ -1,11 +1,14 @@
 import management from '../lib/management';
 import notifications from '../lib/notifications';
+import tabs from '../lib/tabs';
 
 function registerCommand() {
   chrome.commands.onCommand.addListener(async (command) => {
     if (command === 'Alt+R') {
       const extensions = await management.getAll();
+      const devExtIds = management.getActiveDevIds(extensions);
       await management.reloadAllDev(extensions);
+      await tabs.reloadAllByUrlMatch(devExtIds);
       notifications.createNotification();
     }
   });
