4d63b91f085bbe01529a9b482cff0d2c87398041
Alexander Dischberg <noxasch@gmail.com>
2021-08-07 01:47:36 +0800

    chore: refactor manifest task

--- gulpfile.js/manifest.task.js
+++ gulpfile.js/manifest.task.js
@@ -21,20 +21,37 @@ function encodeManifestKey64(fileString, debugKeySting) {
   return Buffer.from(JSON.stringify(res, null, '\t'));
 }
 
+function includeDebugKey(contentsBuffer) {
+  let processBuffer = Buffer.from(contentsBuffer);
+  if (!production) {
+    const debugKey = fs
+      .readFileSync(path.join(process.cwd(), 'assets/debug.pem'))
+      .toString();
+    processBuffer = encodeManifestKey64(processBuffer.toString(), debugKey);
+  } else if (process.env.CI) {
+    processBuffer = encodeManifestKey64(
+      processBuffer.toString(),
+      process.env.PEM_KEY,
+    );
+  }
+  return processBuffer;
+}
+
 function manifestTask() {
   return src(['assets/manifest.json'])
     .pipe(through2.obj(async function (file, _, cb) {
-      if (!production) {
-        const debugKey = fs
-          .readFileSync(path.join(process.cwd(), 'assets/debug.pem'))
-          .toString();
-        file.contents = encodeManifestKey64(file.contents.toString(), debugKey);
-      } else if (process.env.CI) {
-        file.contents = encodeManifestKey64(
-          file.contents.toString(),
-          process.env.PEM_KEY,
-        );
-      }
+      // if (!production) {
+      //   const debugKey = fs
+      //     .readFileSync(path.join(process.cwd(), 'assets/debug.pem'))
+      //     .toString();
+      //   file.contents = encodeManifestKey64(file.contents.toString(), debugKey);
+      // } else if (process.env.CI) {
+      //   file.contents = encodeManifestKey64(
+      //     file.contents.toString(),
+      //     process.env.PEM_KEY,
+      //   );
+      // }
+      file.contents = includeDebugKey(file.contents);
       cb(null, file);
     }))
     .pipe(dest('dist/debug'));
